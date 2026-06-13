2774064bf665225fe98ad72071549237b25e676d
Захар Раджабов <zaipulla1989@gmail.com>
2026-06-13 11:41:59 +0300

    Start_After_Changes_Tesser

--- app/services/ocr_service.py
+++ app/services/ocr_service.py
@@ -1,7 +1,26 @@
+from PIL import Image
+import pytesseract
+
 class OCRService:
+
     def extract_text(self, image_path: str) -> str:
-        """
-        Временная заглушка.
-        Позже здесь будет pytesseract.
-        """
-        return f"OCR text from {image_path}"
+        return pytesseract.image_to_string(Image.open(image_path), lang="rus+eng")
+
+
+
+# class OCRService: после поключения Джанго
+#
+#     def extract_text(self, image_path: str) -> str:
+#         image = Image.open(image_path)
+#         text = pytesseract.image_to_string(image, lang="eng")
+#         return text
+
+
+
+# class OCRService:
+#     def extract_text(self, image_path: str) -> str:
+#         """
+#         Временная заглушка.
+#         Позже здесь будет pytesseract.
+#         """
+#         return f"OCR text from {image_path}"
