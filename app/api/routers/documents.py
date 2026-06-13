9a6788c36bd5931b1cddfa0fad873e42bf9f09a0
Захар Раджабов <zaipulla1989@gmail.com>
2026-06-13 10:32:09 +0300

    Start_After_Changes

--- app/api/routers/documents.py
+++ app/api/routers/documents.py
@@ -1,8 +1,6 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from app.schemas.analyze import (AnalyzeDocumentRequest,AnalyzeDocumentResponse)
 
-from app.services.document_analyze_service import (DocumentAnalyzeService)
-from app.dependencies.depend_services import (get_document_analyze_service)
 from app.tasks.analyze_doc_task import analyze_doc_task
 
 
@@ -14,46 +12,8 @@ router = APIRouter(tags=["Documents"])
 )
 def analyze_doc(
     request: AnalyzeDocumentRequest,
-    document_service: DocumentAnalyzeService = Depends(get_document_analyze_service),
-    # ocr_service: OCRService = Depends(get_ocr_service),
-    # django_repository: DjangoRepository = Depends(get_django_repository),
-    # django_client: DjangoClient = Depends(get_django_client),
-):
+    ):
     task = analyze_doc_task.delay(request.image_id)
 
     return AnalyzeDocumentResponse(detail="Document analysis started", task_id=task.id)
 
-
-
-
-# @router.post(
-#     "/analyze_doc",
-#     response_model=AnalyzeDocumentResponse,
-# )
-# def analyze_doc(
-#     request: AnalyzeDocumentRequest,
-#     ocr_service: OCRService = Depends(get_ocr_service),
-# ):
-#     text = ocr_service.extract_text(
-#         f"image_{request.image_id}.jpg"
-#     )
-#
-#     return AnalyzeDocumentResponse(
-#         detail=text,
-#         task_id="temporary-task-id",
-#     )
-
-
-
-
-
-# from fastapi import APIRouter
-#
-# router = APIRouter(tags=["Documents"])
-#
-#
-# @router.get("/health")
-# async def health_check():
-#     return {
-#         "detail": "Service is running"
-#     }
