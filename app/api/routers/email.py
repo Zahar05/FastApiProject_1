496b70b03ba99d5044a08f26f3d73b396894ef91
Захар Раджабов <zaipulla1989@gmail.com>
2026-06-13 10:45:51 +0300

    Start_After_Changes

--- app/api/routers/email.py
+++ app/api/routers/email.py
@@ -1,9 +1,28 @@
 from fastapi import APIRouter
-
-from app.schemas.email import (SendEmailRequest, SendEmailResponse)
+from app.schemas.email import (SendEmailRequest, SendEmailResponse,)
+from app.tasks.send_email_task import send_email_task
 
 router = APIRouter(tags=["Email"])
 
+
 @router.post("/send_message_to_email", response_model=SendEmailResponse)
 def send_message_to_email(request: SendEmailRequest):
-    return SendEmailResponse(detail=f"Email scheduled for {request.email}")
+    task = send_email_task.delay(
+        request.email,
+        request.subject,
+        request.message,
+    )
+
+    return SendEmailResponse(detail=f"Email task started: {task.id}")
+
+
+
+# from fastapi import APIRouter
+#
+# from app.schemas.email import (SendEmailRequest, SendEmailResponse)
+#
+# router = APIRouter(tags=["Email"])
+#
+# @router.post("/send_message_to_email", response_model=SendEmailResponse)
+# def send_message_to_email(request: SendEmailRequest):
+#     return SendEmailResponse(detail=f"Email scheduled for {request.email}")
