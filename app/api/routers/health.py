8e630b4bbabb50c107352a73a9d48909e2242018
Zaipulla_Radzhabov <zaipulla1989@gmail.com>
2026-07-08 10:48:40 +0300

    New Change

--- app/api/routers/health.py
+++ app/api/routers/health.py
@@ -6,4 +6,4 @@ router = APIRouter(tags=["Health"])
 @router.get("/health")
 def health():
 
-    return {"status": True, "service": "FastAPI", "version": "2.0.0"}
+    return {"status": True, "service": "Fast", "version": "1.0"}
